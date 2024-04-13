1d960c88261089909fae6a1291a4881f4e7fd576
Nameless <76464536+nexor-source@users.noreply.github.com>
2024-04-14 05:18:43 +0800

    v0.0.7

--- MySecondBepInExMod/Class1.cs
+++ MySecondBepInExMod/Class1.cs
@@ -15,13 +15,12 @@ namespace ControllableExplosiveEgg
     {
         private const string modGUID = "nexor.ControllableExplosiveEgg";
         private const string modName = "ControllableExplosiveEgg";
-        private const string modVersion = "0.0.5";
+        private const string modVersion = "0.0.7";
         
         private readonly Harmony harmony = new Harmony(modGUID);
         public ConfigEntry <float> my_explode_chance;
         public ConfigEntry <bool> my_explode_prediction;
         public ConfigEntry <bool> my_better_egg;
-        public bool debug = true;
         public static ControllableExplosiveEgg Instance;
 
         // 在插件启动时会直接调用Awake()方法
@@ -32,29 +31,29 @@ namespace ControllableExplosiveEgg
                 Instance = this;
             }
 
-            // 使用Debug.Log()方法来将文本输出到控制台
+            // 使用// Debug.Log()方法来将文本输出到控制台
             my_better_egg = ((BaseUnityPlugin)this).Config.Bind<bool>("Controllable Explosive Egg Config",
                 "Whether to enable better egg mode",
                 true,
-                "是否启用更好的彩蛋模式，即G键丢下彩蛋不爆炸，左键丢出彩蛋必爆炸 (仅对自己有效)\n" +
-                "Whether to enable a better Easter egg mode, that is, the G button will not explode when the Easter egg is dropped, and the left-click will explode when the Easter egg is thrown (only works on yourself)");
+                "是否启用更好的彩蛋模式，即G键丢下彩蛋不爆炸，左键丢出彩蛋必爆炸 (仅做server时对自己有效)\n" +
+                "Whether to enable a better Easter egg mode, that is, the G button will not explode when the Easter egg is dropped, and the left-click will explode when the Easter egg is thrown (only server & only works on yourself)");
 
             my_explode_chance = ((BaseUnityPlugin)this).Config.Bind<float>("Controllable Explosive Egg Config",
                 "Egg Explode Chance (%)", 
                 16, 
                 "你可以在这里修改全局的彩蛋爆炸的概率 (仅做server时有效)\n" +
-                "You can modify the probability of every egg's exploding here (only works on server)");
+                "You can modify the probability of everyone's egg's exploding here (only server)");
 
             my_explode_prediction = ((BaseUnityPlugin)this).Config.Bind<bool>("Controllable Explosive Egg Config",
                 "Explode Prediction",
                 true,
-                "当彩蛋出现在手上时游戏会计算该彩蛋是否会爆炸，是否将结果展示给你？(仅对自己有效) 如果开启了更好的彩蛋模式，则自动忽略本项\n" +
-                "When the Easter egg appears in hand, the game accountant calculates whether the egg will explode and displays the result to you? (only works on yourself). If Better Easter Egg Mode is enabled, this item will be automatically ignored");
+                "当彩蛋出现在手上时游戏会计算该彩蛋是否会爆炸，是否将结果展示给你？(仅对自己有效，且如果自己不是server则可能会误报但不会漏报) 如果更好的彩蛋模式正常工作，则自动忽略本项\n" +
+                "When the Easter egg appears in hand, the game accountant calculates whether the egg will explode and displays the result to you? (only works on yourself, and if you are not a server, you may have false positives but no false negatives). If Better Easter Egg Mode is working fine, this item will be automatically ignored");
             
 
             if (my_explode_chance.Value >= 99.99f) my_explode_chance.Value = 99.99f;
             harmony.PatchAll();
-            ((ControllableExplosiveEgg)this).Logger.LogInfo((object)"ControllableExplosiveEgg 0.0.5 loaded.");
+            ((ControllableExplosiveEgg)this).Logger.LogInfo((object)"ControllableExplosiveEgg 0.0.7 loaded.");
         }
     }
 }
@@ -67,85 +66,71 @@ namespace ControllableExplosiveEgg.Patches.Items
     internal class StunGrenadeItem_EquipItem_Patch
     {
         [HarmonyPrefix] // 前置补丁
-        // 如果是彩蛋，就设置概率为xxx
         private static void Prefix(StunGrenadeItem __instance)
         {
-            if (__instance.dontRequirePullingPin)
+
+            // 仅作为服务端时，如果有人拿起了一个物品且这个物品是蛋
+            NetworkManager networkManager = __instance.NetworkManager;
+            if (networkManager.IsServer && __instance.dontRequirePullingPin)
             {
-                // 将 chanceToExplode 的值设为 x
+
+                // 将 爆炸概率 设为 x
                 __instance.chanceToExplode = ControllableExplosiveEgg.Instance.my_explode_chance.Value;
-                
-                // Debug.Log("set Chance to explode to: " + __instance.chanceToExplode);
+
+                // Debug.Log("equiped! set Chance to explode to: " + __instance.chanceToExplode);
             }
-            /*else
+/*            else
             {
-                Debug.Log("Chance to explode: " + __instance.chanceToExplode);
+                Debug.Log("equiped! Chance to explode: " + __instance.chanceToExplode);
             }*/
         }
     }
 
-    [HarmonyPatch(typeof(PlayerControllerB))]
-    [HarmonyPatch("GrabObject")]
-    internal class PlayerControllerB_GrabObject_Patch
+    [HarmonyPatch(typeof(StunGrenadeItem))]
+    [HarmonyPatch("SetExplodeOnThrowClientRpc")]
+    internal class StunGrenadeItem_SetExplodeOnThrowClientRpc_Patch
     {
         [HarmonyPostfix]
-        private static void Postfix(PlayerControllerB __instance)
+        private static void Postfix(StunGrenadeItem __instance, bool explode)
         {
-            if (!ControllableExplosiveEgg.Instance.my_better_egg.Value && ControllableExplosiveEgg.Instance.my_explode_prediction.Value && __instance == StartOfRound.Instance.localPlayerController)
+            // 如果没有启用 有效的 更好的蛋模式，才会进行预测
+            NetworkManager networkManager = __instance.NetworkManager;
+            if (!(ControllableExplosiveEgg.Instance.my_better_egg.Value && networkManager.IsServer))
             {
-                // Debug.Log("item grabbed, check if it is stungrenade");
-                FieldInfo currentlyGrabbingObjectField = typeof(PlayerControllerB).GetField("currentlyGrabbingObject", BindingFlags.NonPublic | BindingFlags.Instance);
-                GrabbableObject currentlyGrabbingObject = (GrabbableObject)currentlyGrabbingObjectField.GetValue(__instance);
 
-                // 检查当前抓取的对象是否是 StunGrenadeItem 类型的实例
-                if (currentlyGrabbingObject is StunGrenadeItem)
+                // Debug.Log("into setexplode client, explode :" + explode);
+                // NetworkManager networkManager = __instance.NetworkManager;
+                // Debug.Log("is server, host, client" + networkManager.IsServer + networkManager.IsHost + networkManager.IsClient);
+
+                // 如果服务器给某个蛋返回了爆炸=true且拿着这个蛋的人是你
+                if (explode && __instance.playerHeldBy == StartOfRound.Instance.localPlayerController)
                 {
-                    // Debug.Log("yes, we got: " + currentlyGrabbingObject.itemProperties.itemName);
-                    // 使用反射获取 explodeOnThrow 字段的值
-                    FieldInfo explodeOnThrowField = typeof(StunGrenadeItem).GetField("explodeOnThrow", BindingFlags.NonPublic | BindingFlags.Instance);
-                    bool explodeOnThrowValue = (bool)explodeOnThrowField.GetValue(currentlyGrabbingObject);
 
-                    // 检查 itemName 是否包含 "egg" 并且 explodeOnThrow 字段为 true
-                    if (currentlyGrabbingObject.itemProperties.itemName.Contains("egg") && explodeOnThrowValue)
-                    {
-                        HUDManager.Instance.DisplayTip("Warning!", "Will explode on next drop!!!");
-                    }
+                    // 根据不同角色提示可能会爆炸的信息
+                    
+                    if (networkManager.IsServer) HUDManager.Instance.DisplayTip("Warning!", "will explode on next drop!!!");
+                    else HUDManager.Instance.DisplayTip("Warning!", "might explode on next drop!!!");
+
                 }
             }
+            
         }
     }
 
-    [HarmonyPatch(typeof(PlayerControllerB))]
-    [HarmonyPatch("SwitchToItemSlot")]
-    internal class PlayerControllerB_SwitchToItemSlot_Patch
+/*    [HarmonyPatch(typeof(StunGrenadeItem))]
+    [HarmonyPatch("SetExplodeOnThrowServerRpc")]
+    internal class StunGrenadeItem_SetExplodeOnThrowServerRpc_Patch
     {
         [HarmonyPostfix]
-        private static void Postfix(PlayerControllerB __instance, int slot)
+        private static void Postfix(StunGrenadeItem __instance)
         {
-            if (!ControllableExplosiveEgg.Instance.my_better_egg.Value && ControllableExplosiveEgg.Instance.my_explode_prediction.Value && __instance == StartOfRound.Instance.localPlayerController)
-            {
-                // Debug.Log("item switched, check if it is stungrenade");
-                FieldInfo currentlyGrabbingObjectField = typeof(PlayerControllerB).GetField("currentlyGrabbingObject", BindingFlags.NonPublic | BindingFlags.Instance);
-                // GrabbableObject currentlyGrabbingObject = (GrabbableObject)currentlyGrabbingObjectField.GetValue(__instance);
+            NetworkManager networkManager = __instance.NetworkManager;
+            Debug.Log("is server, host, client" + networkManager.IsServer + networkManager.IsHost + networkManager.IsClient);
 
-                GrabbableObject switched_item = __instance.ItemSlots[slot];
-                // 检查当前抓取的对象是否是 StunGrenadeItem 类型的实例
-                if (switched_item is StunGrenadeItem)
-                {
-                    // Debug.Log("yes, we got: " + switched_item.itemProperties.itemName);
-                    // 使用反射获取 explodeOnThrow 字段的值
-                    FieldInfo explodeOnThrowField = typeof(StunGrenadeItem).GetField("explodeOnThrow", BindingFlags.NonPublic | BindingFlags.Instance);
-                    bool explodeOnThrowValue = (bool)explodeOnThrowField.GetValue(switched_item);
-
-                    // 检查 itemName 是否包含 "egg" 并且 explodeOnThrow 字段为 true
-                    if (switched_item.itemProperties.itemName.Contains("egg") && explodeOnThrowValue)
-                    {
-                        HUDManager.Instance.DisplayTip("Warning!", "Will explode on next drop!!!");
-                    }
-                }
-            }
+            Debug.Log("into setexplode server");
         }
-    }
+    }*/
+
 
     [HarmonyPatch(typeof(PlayerControllerB))]
     [HarmonyPatch("DiscardHeldObject")]
@@ -154,30 +139,62 @@ namespace ControllableExplosiveEgg.Patches.Items
         [HarmonyPrefix]
         private static void Prefix(PlayerControllerB __instance, bool placeObject = false)
         {
-            if (ControllableExplosiveEgg.Instance.my_better_egg.Value && __instance == StartOfRound.Instance.localPlayerController)
+
+            // 如果启用了更好的蛋模式并且自己是server，当有人丢下物品且这个人是你时
+            NetworkManager networkManager = __instance.NetworkManager;
+            if ((ControllableExplosiveEgg.Instance.my_better_egg.Value && networkManager.IsServer) && __instance == StartOfRound.Instance.localPlayerController)
             {
+
                 // Debug.Log("discard held object");
                 // Debug.Log("placeObject:" + placeObject);
                 // Debug.Log("is stungrenade? is egg?"+(__instance.currentlyHeldObjectServer is StunGrenadeItem) + __instance.currentlyHeldObjectServer.itemProperties.itemName.Contains("egg"));
-                if (__instance.currentlyHeldObjectServer is StunGrenadeItem && __instance.currentlyHeldObjectServer.itemProperties.itemName.Contains("egg") && __instance == StartOfRound.Instance.localPlayerController)
+                
+                // 如果丢下的物品是蛋
+                if (__instance.currentlyHeldObjectServer is StunGrenadeItem && __instance.currentlyHeldObjectServer.itemProperties.itemName.Contains("egg"))
                 {
-                    FieldInfo explodeOnThrowField = typeof(StunGrenadeItem).GetField("explodeOnThrow", BindingFlags.NonPublic | BindingFlags.Instance);
                     // 如果丢出去，则爆炸
                     if (placeObject)
                     {
-                        // Debug.Log("EXPLODE!");
-                        explodeOnThrowField.SetValue(__instance.currentlyHeldObjectServer, true);
+                        ((StunGrenadeItem)__instance.currentlyHeldObjectServer).chanceToExplode = 99.99f;
                     }
                     // 如果丢地上，则不爆炸
                     else
                     {
-                        // Debug.Log("WONT EXPLODE!");
-                        explodeOnThrowField.SetValue(__instance.currentlyHeldObjectServer, false);
+                        ((StunGrenadeItem)__instance.currentlyHeldObjectServer).chanceToExplode = 0f;
                     }
+                    // 将更改后的爆炸信息广播给所有玩家
+                    ((StunGrenadeItem)__instance.currentlyHeldObjectServer).SetExplodeOnThrowServerRpc();
                 }
             }
             
         }
     }
 
+/*    [HarmonyPatch(typeof(StunGrenadeItem))]
+    [HarmonyPatch("SetExplodeOnThrowServerRpc")]
+    internal class StunGrenadeItem_SetExplodeOnThrowServerRpc_Patch
+    {
+        [HarmonyPrefix]
+        private static void Prefix(StunGrenadeItem __instance)
+        {
+
+            // Debug.Log("into server setexplode");
+            // Debug.Log("chance to explode is :" + __instance.chanceToExplode);
+        }
+
+        [HarmonyPostfix]
+        private static void Postfix(StunGrenadeItem __instance)
+        {
+
+            // Debug.Log("server setexplode ended");
+            // Debug.Log("chance to explode is :" + __instance.chanceToExplode);
+            FieldInfo explodeOnThrowField = typeof(StunGrenadeItem).GetField("explodeOnThrow", BindingFlags.NonPublic | BindingFlags.Instance);
+            bool explodeOnThrowValue = (bool)explodeOnThrowField.GetValue(__instance);
+            // Debug.Log("will explode? :" + explodeOnThrowValue);
+            // Debug.Log("broadcasting...");
+        }
+    }
+*/
+
+    
 }
